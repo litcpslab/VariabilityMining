6a1951ce7a4bbf40677af88ff38004e2d2bf1bea
Alexander Stummer <49480300+RisingAlex@users.noreply.github.com>
2024-11-08 12:07:51 +0100

    Added code for initial extraction & filtering

---
+++ at.jku.cps.varMining/src/main/java/at/jku/cps/varMining/IVariability.java
@@ -0,0 +1,11 @@
+package at.jku.cps.varMining;
+/*
+*Copyright (c) 2024 Johannes Kepler University Linz*
+*Contributors:
+*Alexander Stummer - initial API and implementation*
+*/
+public interface IVariability {
+	
+	String getLocation();
+
+}
---
+++ at.jku.cps.varMining/src/main/java/at/jku/cps/varMining/IVariant.java
@@ -0,0 +1,12 @@
+package at.jku.cps.varMining;
+/*
+*Copyright (c) 2024 Johannes Kepler University Linz*
+*Contributors:
+*Alexander Stummer - initial API and implementation*
+*/
+public interface IVariant {
+
+	abstract String getName();
+	
+	abstract String getPath();
+}
