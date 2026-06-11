200c2a80dea9a3d0d5c48078f04c44a02c7a2a79
Sophie Öttl <sophie.oettl7@gmail.com>
2026-06-11 15:01:01 +0200

    fixed AddConstraintChildSet

--- VariabilityMining/variability-gui/src/main/java/at/variabilityanalysisgui/changeTracking/AddConstraintChildSet.java
+++ VariabilityMining/variability-gui/src/main/java/at/variabilityanalysisgui/changeTracking/AddConstraintChildSet.java
@@ -8,21 +8,22 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
 import variabilityMining.Feature;
 
+import java.util.Comparator;
 import java.util.Set;
 
 public class AddConstraintChildSet implements ChangeModel<ConstraintsViewController, ConstraintInfoController> {
     Feature feature;
     Group oldGroup;
     Group newGroup;
-    Set<Constraint> constraints;
+    Set<Constraint> keepConstraints;
     int comboBoxIndex;
 
 
-    public AddConstraintChildSet(Feature feature, Group oldGroup, Group newGroup, Set<Constraint> constraints, int comboBoxIndex) {
+    public AddConstraintChildSet(Feature feature, Group oldGroup, Group newGroup, Set<Constraint> keepConstraints, int comboBoxIndex) {
         this.feature = feature;
         this.oldGroup = oldGroup;
         this.newGroup = newGroup;
-        this.constraints = constraints;
+        this.keepConstraints = keepConstraints;
         this.comboBoxIndex = comboBoxIndex;
     }
 
@@ -30,7 +31,7 @@ public class AddConstraintChildSet implements ChangeModel<ConstraintsViewControl
     public void undo(ConstraintsViewController controller, ConstraintInfoController viewController) {
         oldGroup.addFeature(feature);
         newGroup.removeFeature(feature);
-        controller.getConstraints().removeAll(constraints);
+        controller.getConstraints().removeAll(keepConstraints);
 
         if (controller.getIsGroupView() && viewController.getInfoPane().isVisible()) {
             controller.getFeatureComboBox().getItems().add(comboBoxIndex, feature);
@@ -38,7 +39,7 @@ public class AddConstraintChildSet implements ChangeModel<ConstraintsViewControl
             controller.getGroupTreeView().refresh();
         } else if (!controller.getIsGroupView()) {
             ObservableList<TreeItem<Constraint>> treeItems = controller.getGroupTreeView().getRoot().getChildren();
-            for (Constraint constraint : constraints) {
+            for (Constraint constraint : keepConstraints) {
                 treeItems.removeIf(item -> item.getValue().equals(constraint));
             }
             controller.getGroupTreeView().refresh();
@@ -49,17 +50,19 @@ public class AddConstraintChildSet implements ChangeModel<ConstraintsViewControl
     public void redo(ConstraintsViewController controller, ConstraintInfoController viewController) {
         oldGroup.removeFeature(feature);
         newGroup.addFeature(feature);
-        controller.getConstraints().addAll(constraints);
+        controller.getConstraints().addAll(keepConstraints);
 
         if (controller.getIsGroupView() && viewController.getInfoPane().isVisible()) {
             controller.getFeatureComboBox().getItems().remove(feature);
             controller.getGroupFeatureListView().getItems().add(feature);
             controller.getGroupTreeView().refresh();
         } else if (!controller.getIsGroupView()) {
-            for (Constraint c : constraints) {
+            for (Constraint c : keepConstraints) {
                 controller.addSimpleConstraintTreeItem(c);
-                controller.getGroupTreeView().refresh();
             }
+            controller.getGroupTreeView().getRoot().getChildren()
+                    .sort(Comparator.comparingInt(item -> controller.getConstraints().stream().toList().indexOf(item.getValue())));
+            controller.getGroupTreeView().refresh();
         }
     }
 }
